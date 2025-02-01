10c672f5f664b44e8429de1ac71cbdbaf278ffc5
Ulyana-Tykhovskaya <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-02-01 21:52:55 +0000

    Deploying to gh-pages from @ Ulyana-Tykhovskaya/goit-js-hw-09@d7abdc0529f95dabffef20ac895826435d336b50 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const a=document.querySelector(".feedback-form"),m="feedback-form-state";let e={email:"",message:""};const t=localStorage.getItem(m);t&&(e=JSON.parse(t),a.elements.email.value=e.email,a.elements.message.value=e.message);
+//# sourceMappingURL=2-form.js.map
