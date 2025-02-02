aba481d2f2c0e75a0e42cbbe443b747f00de3fad
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-02-02 12:04:19 +0100

    initial

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -10,3 +10,18 @@ if (savedData) {
   form.elements.email.value = formData.email;
   form.elements.message.value = formData.message;
 }
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log('Form Data:', formData);
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
